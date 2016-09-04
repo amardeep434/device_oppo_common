802f765a7e265d27e1f9958d521f5b9355fdb424
Martinusbe <martinusbe@gmail.com>
2016-09-04 11:20:13 +0200

    disable releasetools

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,6 +28,6 @@ TARGET_INIT_VENDOR_LIB := libinit_msm_oppo
 TARGET_POWERHAL_VARIANT := qcom
 
 # Releasetools
-TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_oppo
-TARGET_RELEASETOOLS_EXTENSIONS := device/oppo/common
+#TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_oppo
+#TARGET_RELEASETOOLS_EXTENSIONS := device/oppo/common
 
